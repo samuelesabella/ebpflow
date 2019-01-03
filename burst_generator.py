3fe66eec2e16ffde40b44541c2359b716f0a05c0
Samuele Sabella <sabella@ntop.org>
2019-01-03 10:03:16 +0100

    Bug fix burst_generator

--- burst_generator.py
+++ burst_generator.py
@@ -9,8 +9,8 @@ import signal
 
 
 # ----- Globals and Termination ----- #
-running = True
 TEST_MSG = 'Hello, eBPF!'
+TERMINATION_MSG = '<END>'
 
 # ----- Argument parsing ----- #
 parser = argparse.ArgumentParser()
@@ -29,14 +29,6 @@ if args.file is not None:
     TEST_MSG = msgfile.read()
     TEST_MSG = TEST_MSG.replace('\n', ' ')
 
-# ************************************** */
-# ===== ===== SIGNAL HANDLER ===== ===== //
-# ************************************** */
-def handle_signint(sig, frame):
-  sys.exit(0)
-signal.signal(signal.SIGINT, handle_signint)
-
-
 # ****************************** */
 # ===== ===== SERVER ===== ===== //
 # ****************************** */
@@ -47,23 +39,21 @@ class server (threading.Thread):
     self.port = t_port
 
   def run(self):
-    i = 0
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    sock.settimeout(.2)
     try:
       server_address = ('localhost', self.port)
       sock.bind(server_address)
       sock.listen(1)
-      global running
-      while running:
+      while True:
         try:
-          cl_socket, client_address = sock.accept()
-          cl_socket.send(TEST_MSG.encode())
-          global client_served 
-          client_served += 1
-        except Exception:
-          pass
+          connection, client_address = sock.accept()
+          data = connection.recv(16)
+        except socket.error, exc:
+          continue 
+        stringdata = data.decode('utf-8')
+        if stringdata==TERMINATION_MSG:
+          break
     finally:
       sock.close()
     
@@ -71,26 +61,11 @@ class server (threading.Thread):
 # ****************************** */
 # ===== ===== CLIENT ===== ===== //
 # ****************************** */
-class AtomicInteger():
-  def __init__(self, t_value=0):
-    self.m_value = t_value
-    self.m_lock = threading.Lock()
-
-  def __add__(self, t_v):
-    with self.m_lock:
-      self.m_value += t_v
-      return self
-
-  def get(self):
-    with self.m_lock:
-      return self.m_value
-
-cnum = AtomicInteger(0)
-
 class client_thread (threading.Thread):
-  def __init__(self, t_addr, t_port):
+  def __init__(self, t_addr, t_port, t_msg):
     threading.Thread.__init__(self)
     self.addr = (t_addr, t_port)
+    self.msg = t_msg
 
   def connect(self):
     s = socket.socket()
@@ -98,11 +73,9 @@ class client_thread (threading.Thread):
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     try:
       s.connect(self.addr)
-      s.recv(len(TEST_MSG) + 1)
-      global cnum
-      cnum += 1
+      s.send(self.msg)
       return True
-    except Exception:
+    except socket.error, exc:
       return False
     finally:
       s.close()
@@ -120,7 +93,7 @@ class atlante_client():
 
   def rise_up(self, t_num):
     for i in range(0, t_num):
-      c = client_thread(self.addr, self.port)
+      c = client_thread(self.addr, self.port, TEST_MSG)
       self.m_tlist.append(c)
       c.start()
 
@@ -139,9 +112,9 @@ ac = atlante_client("localhost", port)
 ac.rise_up(CLIENT_NUM)
 
 ac.join()
+term_thread = client_thread("localhost", port, TERMINATION_MSG)
+term_thread.start()
+term_thread.join()
 print('Client terminated, closing server')
-running = False
 s.join()
 
-print('Served: %s' % (client_served))
-print('Connections: %s' % (cnum.get()))
